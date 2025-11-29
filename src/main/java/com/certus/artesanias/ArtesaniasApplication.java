6c63c642f2332571f3fd9935d4abd2be220ce50a
Nilperalta <nildalisset.ll@gmail.com>
2025-11-29 18:10:33 -0500

    hasheado de admin listo

--- src/main/java/com/certus/artesanias/ArtesaniasApplication.java
+++ src/main/java/com/certus/artesanias/ArtesaniasApplication.java
@@ -2,12 +2,16 @@ package com.certus.artesanias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootApplication
 public class ArtesaniasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArtesaniasApplication.class, args);
+		//BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    	//System.out.println(encoder.encode("Admin123#"));
 	}
 
 }
